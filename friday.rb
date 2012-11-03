72fb682f5cf5fa59327bf82f371a06fdb24c2c5a
Ryan Rix <ry@n.rix.si>
2012-11-03 01:12:59 -0700

    Add minutes generation to friday friday gotta get down

--- friday.rb
+++ friday.rb
@@ -11,16 +11,26 @@ def get_config
 end
 #}}}1
 
+@config = get_config
+
 # This script runs every Friday; if the last meeting was yesterday we need
 # to send minutes; otherwise we need to send out the agenda'd proposals and
 # set up the wiki pages since voting is in 6 days
 if last_meeting == Date.today - 1
+  url = "#{@config['wiki']['wiki']['url']}#{last_meeting.to_s}"
+  binding.pry
 
+  send_minutes_for url
+  exit
 elsif next_meeting == Date.today + 6
-  @config = get_config
   wiki_page = premeeting_wiki_generation @config['wiki']
 
   # mailer
   send_agenda_for wiki_page, messages_since(last_meeting - 7), @config['mailer']
+
+  exit
 end
+
+puts "I shouldn't be here, kill me!"
+exit
 
