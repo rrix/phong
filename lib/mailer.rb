113b4f03d445c74ec98756c33c9065d67126486d
Ryan Rix <ry@n.rix.si>
2012-11-02 19:57:06 -0700

    Make the reminder mail have a progress spinner

--- lib/mailer.rb
+++ lib/mailer.rb
@@ -1,25 +1,48 @@
+# vim: set foldmethod=marker foldlevel=0:
 require 'net/smtp'
 
-def send_reminder_mail_for wiki_page, config
-  @mailer_config = config
-  template = config['template']
-
-  templated =  template.gsub(/\$URL\$/i, wiki_page)
-  templated = templated.gsub(/\$GREETING\$/i, greeting)
-
-  header = generate_header
-
-  full_mail = header + templated
-
-  smtp = Net::SMTP.new(config['smtp']['server'],
-                       config['smtp']['port'])
-  smtp.enable_starttls
-  smtp.start( 'localhost',
-              config['smtp']['username'],
-              config['smtp']['password'],
-              :plain) do
-    smtp.send_message full_mail, config['smtp']['username'], config['mailing_list']
+#{{{1 Magick
+def show_wait_spinner(fps=5)
+  chars = %w[ | / - \\ ]
+  delay = 1.0/fps
+  iter = 0
+  spinner = Thread.new do
+    while iter do  # Keep spinning until told otherwise
+      print chars[(iter+=1) % chars.length]
+      sleep delay
+      print "\b"
+    end
   end
+  yield.tap{       # After yielding to the block, save the return value
+    iter = false   # Tell the thread to exit, cleaning up after itself…
+    spinner.join   # …and wait for it to do so.
+  }                # Use the block's return value as the method's
+end
+#1}}}
+
+def send_reminder_mail_for wiki_page, config
+  print "*** Sending mail ... "
+  show_wait_spinner{
+    @mailer_config = config
+    template = config['template']
+
+    templated =  template.gsub(/\$URL\$/i, wiki_page)
+    templated = templated.gsub(/\$GREETING\$/i, greeting)
+
+    header = generate_header
+
+    full_mail = header + templated
+
+    smtp = Net::SMTP.new(config['smtp']['server'],
+                         config['smtp']['port'])
+    smtp.enable_starttls if config['smtp']['ssl']
+    smtp.start( 'localhost',
+                config['smtp']['username'],
+                config['smtp']['password'],
+                :plain) do
+      smtp.send_message full_mail, config['smtp']['username'], config['mailing_list']
+    end
+  }
 
   puts 'Message sent to mailing list!'
 end
