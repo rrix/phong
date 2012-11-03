55094716eef22f6596c2216d485bb821040ab38c
Ryan Rix <ry@n.rix.si>
2012-11-03 01:01:13 -0700

    Add ability to generate and send agenda mails (DRY-er too)

--- lib/mailer.rb
+++ lib/mailer.rb
@@ -20,42 +20,75 @@ def show_wait_spinner(fps=5)
 end
 #1}}}
 
-def send_reminder_mail_for wiki_page, config
-  print "*** Sending mail ... "
+def send_agenda_for url, stuffed_envelopes, config
+  print "*** Sending agenda mail ... "
   show_wait_spinner{
     @mailer_config = config
-    template = config['template']
+
+    template = config['agenda_template']
+
+    subjects = stuffed_envelopes.map{|prop| prop['ENVELOPE'].subject} * ", "
+
+    templated =  template.gsub(/\$URL\$/i, wiki_page)
+    templated = templated.gsub(/\$GREETING\$/i, greeting)
+    templated = templated.gsub(/\$PROPOSALS\$/i, subjects)
+
+    header = generate_agenda_header
+    full_mail = header + templated
+
+    send_message full_mail
+  }
+end
+
+def send_reminder_mail_for wiki_page, config
+  print "*** Sending reminder mail ... "
+  show_wait_spinner{
+    @mailer_config = config
+    template = config['reminder_template']
 
     templated =  template.gsub(/\$URL\$/i, wiki_page)
     templated = templated.gsub(/\$GREETING\$/i, greeting)
 
-    header = generate_header
+    header = generate_reminder_header
 
     full_mail = header + templated
 
-    smtp = Net::SMTP.new(config['smtp']['server'],
-                         config['smtp']['port'])
-    smtp.enable_starttls if config['smtp']['ssl']
-    smtp.start( 'localhost',
-                config['smtp']['username'],
-                config['smtp']['password'],
-                :plain) do
-      smtp.send_message full_mail, config['smtp']['username'], config['mailing_list']
-    end
+    send_message full_mail
   }
 
   puts 'Message sent to mailing list!'
 end
 
-def generate_header
+def send_message full_mail
+    smtp = Net::SMTP.new(@mailer_config['smtp']['server'],
+                         @mailer_config['smtp']['port'])
+    smtp.enable_starttls if @mailer_config['smtp']['ssl']
+    smtp.start( 'localhost',
+                @mailer_config['smtp']['username'],
+                @mailer_config['smtp']['password'],
+                :plain) do
+      smtp.send_message full_mail, @mailer_config['smtp']['username'], @mailer_config['mailing_list']
+    end
+end
+
+def generate_base_header
   str = <<HEREDOC
 From: Phong <#{@mailer_config['smtp']['username']}>
 To: #{@mailer_config['mailing_list']}
-Subject: Hack Your Hackerspace Meeting Tomorrow!
 Date: #{DateTime.now.to_s}
-
 HEREDOC
+end
 
+def generate_agenda_header
+  header = generate_base_header + "Subject: Hack Your Hackerspace Proposals for next week are in!
+
+  "
+end
+
+def generate_reminder_header
+  header = generate_base_header + "Subject: Hack Your Hackerspace Meeting Tomorrow!
+
+  "
 end
 
 def greeting
@@ -64,7 +97,8 @@ def greeting
     "Greetings!",
     "Hackers, Makers, Doers and Breakers!",
     "Salut, hackers!",
-    "INCOMING TRANSMISSION:"
+    "INCOMING TRANSMISSION:",
+    "Namaste hackers!"
   ]
 
   possibilities[(rand*possibilities.count).to_i]
