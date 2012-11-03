a9d53e7bb3fca5035fb9062eddf008d2f6495537
Ryan Rix <ry@n.rix.si>
2012-11-02 19:56:50 -0700

    Add date check and switch to prod mode

--- premeeting.rb
+++ premeeting.rb
@@ -10,9 +10,17 @@ def get_config
 end
 #}}}1
 
-@config = get_config
-#wiki_page = premeeting_wiki_generation @config['wiki']
+if next_meeting != Date.today + 1
+  # This script runs every wednesday, but we don't have meetings that often, we need to check for that because cron (move to atd?)
+  print "Must be an off-week. Our next meeting is #{next_meeting.to_s}, not tomorrow (which is #{(Date.today + 1).to_s})!\n"
+  exit 0
+end
 
-#send_reminder_mail_for wiki_page, @config['mailer']
-print send_reminder_mail_for "http://wiki.heatsynclabs.org/wiki/HYH_Meeting_2012-11-08", @config['mailer']
+@config = get_config
+wiki_page = premeeting_wiki_generation @config['wiki']
+
+send_reminder_mail_for wiki_page, @config['mailer']
+
+#testing
+#print send_reminder_mail_for "http://wiki.heatsynclabs.org/wiki/HYH_Meeting_2012-11-08", @config['mailer']
 
